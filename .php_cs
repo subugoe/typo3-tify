5e95bb904fc3dd21ce02d0be596bf26ff148a3d2
Ingo Pfennigstorf <i.pfennigstorf@gmail.com>
2017-03-29 11:08:31 +0200

    Initial import

---
+++ .php_cs
@@ -0,0 +1,18 @@
+<?php
+
+$finder = PhpCsFixer\Finder::create()
+    ->exclude('build')
+    ->exclude('cache')
+    ->exclude('var')
+    ->exclude('vendor')
+    ->in(__DIR__);
+
+$config = PhpCsFixer\Config::create();
+$config
+    ->setRules([
+        '@Symfony' => true,
+        'array_syntax' => ['syntax' => 'short'],
+    ])
+    ->setFinder($finder);
+
+return $config;
---
+++ ext_emconf.php
@@ -0,0 +1,20 @@
+<?php
+
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'tify',
+    'description' => 'tify integration for TYPO3',
+    'category' => 'frontend',
+    'version' => '1.0.0',
+    'state' => 'stable',
+    'clearcacheonload' => 1,
+    'author' => 'Ingo Pfennigstorf',
+    'author_email' => 'pfennigstorf@sub.uni-goettingen.de',
+    'author_company' => 'SUB Uni-Göttingen',
+    'constraints' => [
+            'depends' => [
+                    'typo3' => '7.6.0-7.99.99',
+                ],
+            'conflicts' => [],
+            'suggests' => [],
+        ],
+];
