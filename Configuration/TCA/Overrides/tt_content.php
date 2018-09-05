7cc676f9173488a3713cd078798f332b8bd6266f
Ingo Pfennigstorf <i.pfennigstorf@gmail.com>
2018-09-05 10:09:56 +0200

    Add TYPO3 9 compatibility

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -1,37 +1,25 @@
 <?php
 
 defined('TYPO3_MODE') || die('Access denied.');
-call_user_func(
-    function ($extKey) {
-        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPlugin(
-            [
-                'Tify IIIF Viewer',
-                $extKey,
-                'content-textmedia',
-            ],
-            'CType',
-            $extKey
-        );
-        $tca = [
-            'types' => [
-                $extKey => [
-                    'showitem' => '
-                        --palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:palette.general;general,
-                        header,
-                        bodytext,Manifest URL,
-                    ',
-                    'columnsOverrides' => [
-                        'bodytext' => [
-                            'config' => [
-                                'rows' => 1,
-                                'eval' => 'required',
-                            ],
-                        ],
-                    ],
-                ],
-            ],
-        ];
-        $GLOBALS['TCA']['tt_content'] = array_replace_recursive($GLOBALS['TCA']['tt_content'], $tca);
-    },
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPlugin(
+    [
+        'Tify IIIF Viewer',
+        'tify_tify',
+        'content-textmedia',
+    ],
+    'CType',
     'tify'
 );
+
+$GLOBALS['TCA']['tt_content']['types']['tify_tify'] = [
+    'showitem' => '--palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:palette.general;general,header,bodytext,Manifest URL,',
+    'columnsOverrides' => [
+        'bodytext' => [
+            'config' => [
+                'rows' => 1,
+                'eval' => 'required',
+            ],
+        ],
+    ],
+];
