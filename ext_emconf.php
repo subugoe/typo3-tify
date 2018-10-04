ae20cfa61ceac108d4eeff2e5d2d22c50a5fb5d9
Ingo Pfennigstorf <i.pfennigstorf@gmail.com>
2018-10-04 08:03:14 +0200

    Allow TYPO3 9.5

--- ext_emconf.php
+++ ext_emconf.php
@@ -12,7 +12,7 @@ $EM_CONF[$_EXTKEY] = [
     'author_company' => 'SUB Uni-Göttingen',
     'constraints' => [
         'depends' => [
-                'typo3' => '8.7.0-8.7.99',
+                'typo3' => '8.7.0-9.5.99',
             ],
         'conflicts' => [],
         'suggests' => [],
