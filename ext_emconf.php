a303257ab3340597111f36d963498b9b8595a9c8
Ingo Pfennigstorf <i.pfennigstorf@gmail.com>
2020-04-22 10:10:36 +0200

    Allow version constraints also in ext_emconf

--- ext_emconf.php
+++ ext_emconf.php
@@ -12,7 +12,7 @@ $EM_CONF[$_EXTKEY] = [
     'author_company' => 'SUB Uni-Göttingen',
     'constraints' => [
         'depends' => [
-                'typo3' => '8.7.0-9.5.99',
+                'typo3' => '9.5.0-10.4.99',
             ],
         'conflicts' => [],
         'suggests' => [],
