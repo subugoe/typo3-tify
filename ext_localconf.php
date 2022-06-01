236e292bb6201942c2e33150f1b5d488341a749a
Ingo Pfennigstorf <i.pfennigstorf@gmail.com>
2022-06-01 09:11:41 +0200

    Update TIFY to 0.25.4

--- ext_localconf.php
+++ ext_localconf.php
@@ -4,7 +4,13 @@ if (!defined('TYPO3_MODE')) {
     exit('Access denied.');
 }
 
-// Add PageTSConfig
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig(
-    '<INCLUDE_TYPOSCRIPT: source="DIR: EXT:tify/Configuration/TSConfig/Page/General" extensions="typoscript">'
-);
+$autoexec = static function () {
+    // Add PageTSConfig
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig(
+        '<INCLUDE_TYPOSCRIPT: source="DIR: EXT:tify/Configuration/TSConfig/Page/General" extensions="typoscript">'
+    );
+};
+
+$autoexec();
+
+unset($autoexec);
